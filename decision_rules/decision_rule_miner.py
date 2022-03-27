c78ab5044cde25dfa876622e71edacc5622d353f
Michael Schulten <michael.schulten@celonis.de>
2022-03-27 20:48:26 +0200

    feat: add module for decision rules

--- decision_rules/decision_rule_miner.py
+++ decision_rules/decision_rule_miner.py
@@ -1,35 +1,62 @@
-import wittgenstein as lw
 import numpy as np
 import pandas as pd
-from sklearn.metrics import confusion_matrix
+import wittgenstein as lw
 from pandas.api.types import is_numeric_dtype
-from errors import MinimumValueReachedError, MaximumValueReachedError
+from sklearn.metrics import confusion_matrix
+
+from errors import MaximumValueReachedError
+from errors import MinimumValueReachedError
+
 
 class DecisionRuleMiner:
-    configs = [{'max_rule_conds': 1, 'max_rules': 1}, {'max_rule_conds': 1, 'max_rules': 2},
-        {'max_rule_conds': 2, 'max_rules': 2}, {'max_rule_conds': 2, 'max_rules': 3},
-        {'max_rule_conds': 2, 'max_rules': 6}, {'max_rule_conds': 3, 'max_rules': 6}]
+    configs = [
+        {"max_rule_conds": 1, "max_rules": 1},
+        {"max_rule_conds": 1, "max_rules": 2},
+        {"max_rule_conds": 2, "max_rules": 2},
+        {"max_rule_conds": 2, "max_rules": 3},
+        {"max_rule_conds": 2, "max_rules": 6},
+        {"max_rule_conds": 3, "max_rules": 6},
+    ]
 
-    def __init__(self, df, class_label, attrbute_labels, pos_class: str = None, threshold: float = None,
-                 config_index:int = 2, n_discretize_bins: int = 3, k:int = 3, random_state:int = 42):
+    def __init__(
+        self,
+        df,
+        class_label,
+        attrbute_labels,
+        pos_class: str = None,
+        threshold: float = None,
+        config_index: int = 2,
+        n_discretize_bins: int = 3,
+        k: int = 3,
+        random_state: int = 42,
+    ):
         self.df = df
         self.attrbute_labels = attrbute_labels
         self.preprocessed_label_col = "Decisionrulelabel"
         self.class_label = class_label
         self.attribute_labels = attrbute_labels
-        self.max_rule_conds = None  # probably shouldn't change this to a higher value since it will become too
+        # probably shouldn't change this to a higher value since it will become too
+        self.max_rule_conds = None
         # difficult to read
-        self.n_discretize_bins = n_discretize_bins  # this should be fixed. Maybe can also use 3
-        self.pos_class = pos_class  # the value of the positive class. Leave at None if class is numerical
+        self.n_discretize_bins = (
+            n_discretize_bins  # this should be fixed. Maybe can also use 3
+        )
+        # the value of the positive class. Leave at None if class is numerical
+        self.pos_class = pos_class
         self.k = k  # number of iterations
         self.random_state = random_state
-        self.max_rules = None  # this variable can be changed to make rules simpler or more elaborate
-
-        self.threshold = threshold  # threshold to define positive class for numerical class. Positive class is >=
+        self.max_rules = (
+            None  # this variable can be changed to make rules simpler or more elaborate
+        )
+        # threshold to define positive class for numerical class. Positive class is
+        # >= threshold
+        self.threshold = threshold
         # threshold
         self.label_df = self.get_label_df()
         self.rules = None
-        self.attr_dict = self._create_attribute_dict()  # dict that maps from the changed names to the original names
+        self.attr_dict = (
+            self._create_attribute_dict()
+        )  # dict that maps from the changed names to the original names
         self.clf = None
         self.structured_rules = None
         self.metrics = {}
@@ -37,10 +64,9 @@ class DecisionRuleMiner:
         self.apply_config()
 
     def apply_config(self):
-        self.max_rule_conds = self.configs[self.config_index]['max_rule_conds']
-        self.max_rules = self.configs[self.config_index]['max_rules']
+        self.max_rule_conds = self.configs[self.config_index]["max_rule_conds"]
+        self.max_rules = self.configs[self.config_index]["max_rules"]
         self.config_index = self.config_index
-
 
     def get_label_df(self):
         """
@@ -52,7 +78,9 @@ class DecisionRuleMiner:
         # create new label column if original is numerical
         if is_numeric_dtype(self.df[self.class_label]) and self.threshold is not None:
 
-            label_df[self.preprocessed_label_col] = np.where(self.df[self.class_label] >= self.threshold, 1, 0)
+            label_df[self.preprocessed_label_col] = np.where(
+                self.df[self.class_label] >= self.threshold, 1, 0
+            )
             self.pos_class = 1
 
         else:
@@ -64,21 +92,45 @@ class DecisionRuleMiner:
         self._fit()
         self.structured_rules = self.create_structured_rules()
         pred = self._get_prediction_labels()
-        (true_n, false_p, false_n, true_p, recall_p, recall_n, precision_p, precision_n,) = self._get_confusion_matrix(
-            pred)
+        (
+            true_n,
+            false_p,
+            false_n,
+            true_p,
+            recall_p,
+            recall_n,
+            precision_p,
+            precision_n,
+        ) = self._get_confusion_matrix(pred)
         avg_True, avg_False = self._get_avg_label_value(pred)
-        metric_dict = {"true_n": true_n, "false_p": false_p, "false_n": false_n, "true_p": true_p, "recall_p": recall_p,
-            "recall_n": recall_n, "precision_p": precision_p, "precision_n": precision_n, "avg_True": avg_True,
-            "avg_False": avg_False, }
-        self. metrics = metric_dict
+        metric_dict = {
+            "true_n": true_n,
+            "false_p": false_p,
+            "false_n": false_n,
+            "true_p": true_p,
+            "recall_p": recall_p,
+            "recall_n": recall_n,
+            "precision_p": precision_p,
+            "precision_n": precision_n,
+            "avg_True": avg_True,
+            "avg_False": avg_False,
+        }
+        self.metrics = metric_dict
 
     def _fit(self):
-        self.clf = lw.RIPPER(max_rule_conds=self.max_rule_conds, n_discretize_bins=self.n_discretize_bins, k=self.k,
-            random_state=self.random_state, max_rules=self.max_rules, )
-        self.clf.fit(pd.concat([self.df[self.attrbute_labels], self.label_df], axis=1),
-            class_feat=self.preprocessed_label_col, pos_class=self.pos_class, )
+        self.clf = lw.RIPPER(
+            max_rule_conds=self.max_rule_conds,
+            n_discretize_bins=self.n_discretize_bins,
+            k=self.k,
+            random_state=self.random_state,
+            max_rules=self.max_rules,
+        )
+        self.clf.fit(
+            pd.concat([self.df[self.attrbute_labels], self.label_df], axis=1),
+            class_feat=self.preprocessed_label_col,
+            pos_class=self.pos_class,
+        )
         self.rules = self.clf.ruleset_
-
 
     def _get_prediction_labels(self):
         pred = self.clf.predict(self.df[self.attrbute_labels])
@@ -91,13 +143,21 @@ class DecisionRuleMiner:
         recall_n = true_n / (true_n + false_p)
         precision_p = true_p / (true_p + false_p)
         precision_n = true_n / (true_n + false_n)
-        return (true_n, false_p, false_n, true_p, recall_p, recall_n, precision_p, precision_n,)
+        return (
+            true_n,
+            false_p,
+            false_n,
+            true_p,
+            recall_p,
+            recall_n,
+            precision_p,
+            precision_n,
+        )
 
     def _get_avg_label_value(self, pred):
         avg_True = self.df[pred][self.class_label].mean()
         avg_False = self.df[~pred][self.class_label].mean()
         return avg_True, avg_False
-
 
     def create_structured_rules(self):
         ruleset = self.clf.ruleset_
@@ -117,13 +177,22 @@ class DecisionRuleMiner:
                 elif isinstance(val, str):
                     unequal_sign = "between"
 
-                conds.append(({'attribute': feature, 'value': str(val), 'unequal_sign': unequal_sign}))
+                conds.append(
+                    (
+                        {
+                            "attribute": feature,
+                            "value": str(val),
+                            "unequal_sign": unequal_sign,
+                        }
+                    )
+                )
             structured_rules.append(conds)
         return structured_rules
 
     def _create_attribute_dict(self):
-        """For the decision miner algorythm, the names of the attributes are changed a bit. So here a dict is created
-        that maps from the changed names to the original names
+        """For the decision miner algorythm, the names of the attributes are changed
+        a bit. So here a dict is created that maps from the changed names to the
+        original names
 
         :return:
         """
@@ -134,7 +203,7 @@ class DecisionRuleMiner:
         return attr_dict
 
     def simplify_rule_config(self):
-        """ Reducing the config by 1
+        """Reducing the config by 1
 
         :return:
         """
@@ -144,9 +213,8 @@ class DecisionRuleMiner:
         else:
             raise MinimumValueReachedError()
 
-
     def elaborate_rule_config(self):
-        """ Reducing the config by 1
+        """Reducing the config by 1
 
         :return:
         """
