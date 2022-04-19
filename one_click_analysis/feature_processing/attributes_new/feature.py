67b66eddb1e43fe3664d2eb8d840f91ac8d84927
Michael Schulten <michael.schulten@celonis.de>
2022-04-19 12:15:37 +0200

    feat: add more static attributes

--- one_click_analysis/feature_processing/attributes_new/feature.py
+++ one_click_analysis/feature_processing/attributes_new/feature.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 
-from one_click_analysis.feature_processing.attributes.attribute import AttributeDataType
+from one_click_analysis.feature_processing.attributes_new.attribute import (
+    AttributeDataType,
+)
 
 
 @dataclass
--- one_click_analysis/feature_processing/attributes_new/static_attributes.py
+++ one_click_analysis/feature_processing/attributes_new/static_attributes.py
@@ -1,11 +1,10 @@
 import abc
-from typing import List
 
 from prediction_builder.data_extraction import ProcessModel
 from pycelonis.celonis_api.pql import pql
 
-from one_click_analysis.feature_processing.attributes import attribute_utils
-from one_click_analysis.feature_processing.attributes.attribute import Attribute
+from one_click_analysis.feature_processing.attributes_new import attribute_utils
+from one_click_analysis.feature_processing.attributes_new.attribute import Attribute
 
 
 class StaticAttribute(Attribute, abc.ABC):
@@ -75,7 +74,7 @@ class WorkInProgressAttribute(StaticAttribute):
         self.aggregation = aggregation
         aggregation_df_name = attribute_utils.get_aggregation_df_name(aggregation)
         self.attribute_name = "Case Work in progress" + " (" + aggregation_df_name + ")"
-        pql_query = self._gen_query(self.attribute_name)
+        pql_query = self._gen_query()
         super().__init__(
             process_model=self.process_model,
             attribute_name=self.attribute_name,
@@ -144,19 +143,19 @@ class EventCountAttribute(StaticAttribute):
         return pql.PQLColumn(query=q, name=self.attribute_name)
 
 
-class ReworkOccurenceAttribute(StaticAttribute):
+class ActivityOccurenceAttribute(StaticAttribute):
     """Rework occurence for a whole case"""
 
     def __init__(
         self,
         process_model: ProcessModel,
-        activity: List[str],
+        activity: str,
         is_feature: bool = False,
         is_class_feature: bool = False,
     ):
         self.process_model = process_model
         self.activity = activity
-        self.attribute_name = f"Activity = {self.activity} (Rework occurence)"
+        self.attribute_name = f"Activity = {self.activity} (occurence)"
         pql_query = self._gen_query()
         super().__init__(
             pql_query=pql_query,
@@ -168,10 +167,212 @@ class ReworkOccurenceAttribute(StaticAttribute):
 
     def _gen_query(self) -> pql.PQLColumn:
         q = (
-            f'CASE WHEN SUM(CASE WHEN "'
+            f'CASE WHEN PU_SUM("{self.process_model.case_table_str}", CASE WHEN "'
             f'{self.process_model.activity_table_str}"."'
             f"{self.process_model.activity_column_str}\" = '{self.activity}' THEN 1 "
             f"ELSE 0"
-            f" END) >= 2 THEN 1 ELSE 0 END"
+            f" END) >= 1 THEN 1 ELSE 0 END"
         )
+        return pql.PQLColumn(query=q, name=self.attribute_name)
+
+
+class ReworkCountAttribute(StaticAttribute):
+    """Count of reworked activities"""
+
+    def __init__(
+        self,
+        process_model: ProcessModel,
+        is_feature: bool = False,
+        is_class_feature: bool = False,
+    ):
+        self.process_model = process_model
+        self.attribute_name = f"Case Rework count (all activities)"
+        pql_query = self._gen_query()
+        super().__init__(
+            pql_query=pql_query,
+            process_model=self.process_model,
+            attribute_name=self.attribute_name,
+            is_feature=is_feature,
+            is_class_feature=is_class_feature,
+        )
+
+    def _gen_query(self) -> pql.PQLColumn:
+        q = f"""
+            PU_SUM("{self.process_model.case_table_str}",
+                CASE
+                    WHEN INDEX_ACTIVITY_TYPE (
+                        {self.process_model.activity_column.query}) > 1
+                    THEN 1
+                    ELSE 0
+                END)
+            """
+        return pql.PQLColumn(query=q, name=self.attribute_name)
+
+
+class ReworkOccurrenceAttribute(StaticAttribute):
+    """Whether any activity was done more than once"""
+
+    def __init__(
+        self,
+        process_model: ProcessModel,
+        is_feature: bool = False,
+        is_class_feature: bool = False,
+    ):
+        self.process_model = process_model
+        self.attribute_name = f"Case Rework occurence (any activity)"
+        pql_query = self._gen_query()
+        super().__init__(
+            pql_query=pql_query,
+            process_model=self.process_model,
+            attribute_name=self.attribute_name,
+            is_feature=is_feature,
+            is_class_feature=is_class_feature,
+        )
+
+    def _gen_query(self) -> pql.PQLColumn:
+        q = f"""
+            CASE
+            WHEN PU_SUM("{self.process_model.case_table_str}",
+                CASE
+                    WHEN INDEX_ACTIVITY_TYPE (
+                        {self.process_model.activity_column.query}) > 1
+                    THEN 1
+                    ELSE 0
+                END) >=1 THEN 1 ELSE 0 END
+            """
+        return pql.PQLColumn(query=q, name=self.attribute_name)
+
+
+class StartActivityAttribute(StaticAttribute):
+    """Start activity"""
+
+    def __init__(
+        self,
+        process_model: ProcessModel,
+        is_feature: bool = False,
+        is_class_feature: bool = False,
+    ):
+        self.process_model = process_model
+        self.attribute_name = f"Start activity"
+        pql_query = self._gen_query()
+        super().__init__(
+            pql_query=pql_query,
+            process_model=self.process_model,
+            attribute_name=self.attribute_name,
+            is_feature=is_feature,
+            is_class_feature=is_class_feature,
+        )
+
+    def _gen_query(self) -> pql.PQLColumn:
+        q = (
+            'PU_FIRST("' + self.process_model.case_table_str + '", '
+            '"'
+            + self.process_model.activity_table_str
+            + '"."'
+            + self.process_model.activity_column_str
+            + '")'
+        )
+        return pql.PQLColumn(query=q, name=self.attribute_name)
+
+
+class EndActivityAttribute(StaticAttribute):
+    """End activity"""
+
+    def __init__(
+        self,
+        process_model: ProcessModel,
+        is_feature: bool = False,
+        is_class_feature: bool = False,
+    ):
+        self.process_model = process_model
+        self.attribute_name = f"End activity"
+        pql_query = self._gen_query()
+        super().__init__(
+            pql_query=pql_query,
+            process_model=self.process_model,
+            attribute_name=self.attribute_name,
+            is_feature=is_feature,
+            is_class_feature=is_class_feature,
+        )
+
+    def _gen_query(self) -> pql.PQLColumn:
+        q = (
+            'PU_LAST("' + self.process_model.case_table_str + '", '
+            '"'
+            + self.process_model.activity_table_str
+            + '"."'
+            + self.process_model.activity_column_str
+            + '")'
+        )
+        return pql.PQLColumn(query=q, name=self.attribute_name)
+
+
+class NumericActivityTableColumnAttribute(StaticAttribute):
+    """Any numeric activity table column."""
+
+    def __init__(
+        self,
+        process_model: ProcessModel,
+        column_name: str,
+        aggregation: str,
+        is_feature: bool = False,
+        is_class_feature: bool = False,
+    ):
+        self.process_model = process_model
+        self.column_name = column_name
+        self.aggregation = aggregation  # aggregation for PU function
+        self.attribute_name = (
+            f"{self.process_model.activity_table_str}."
+            f"{self.column_name} ("
+            f"{attribute_utils.get_aggregation_df_name(aggregation)})"
+        )
+        pql_query = self._gen_query()
+        super().__init__(
+            pql_query=pql_query,
+            process_model=self.process_model,
+            attribute_name=self.attribute_name,
+            is_feature=is_feature,
+            is_class_feature=is_class_feature,
+        )
+
+    def _gen_query(self) -> pql.PQLColumn:
+        q = (
+            "PU_" + self.aggregation + '("' + self.process_model.case_table_str + '", '
+            '"'
+            + self.process_model.activity_table_str
+            + '"."'
+            + self.column_name
+            + '")'
+        )
+        return pql.PQLColumn(query=q, name=self.attribute_name)
+
+
+class CaseTableColumnAttribute(StaticAttribute):
+    """Any case table column."""
+
+    def __init__(
+        self,
+        process_model: ProcessModel,
+        column_name: str,
+        aggregation: str,
+        is_feature: bool = False,
+        is_class_feature: bool = False,
+    ):
+        self.process_model = process_model
+        self.column_name = column_name
+        self.aggregation = aggregation  # aggregation for PU function
+        self.attribute_name = (
+            f"{self.process_model.case_table_str}." f"{self.column_name}"
+        )
+        pql_query = self._gen_query()
+        super().__init__(
+            pql_query=pql_query,
+            process_model=self.process_model,
+            attribute_name=self.attribute_name,
+            is_feature=is_feature,
+            is_class_feature=is_class_feature,
+        )
+
+    def _gen_query(self) -> pql.PQLColumn:
+        q = f'"{self.process_model.case_table_str}"."{self.column_name}"'
         return pql.PQLColumn(query=q, name=self.attribute_name)
