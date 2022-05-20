967c1ec60df8ff68ab77cd2b611f9b5636d3129b
Michael Schulten <michael.schulten@celonis.de>
2022-05-20 19:33:24 +0200

    feat: add configuration for attribute selection

--- one_click_analysis/configuration/configurations_new.py
+++ one_click_analysis/configuration/configurations_new.py
@@ -1,8 +1,13 @@
 import abc
-from typing import Any, Optional, List, Callable, Tuple
+import functools
+from typing import Any
+from typing import Callable
 from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Tuple
 
-from ipywidgets import Box, widgets
+from ipywidgets import Box
 from ipywidgets import Checkbox
 from ipywidgets import DatePicker
 from ipywidgets import HBox
@@ -10,17 +15,19 @@ from ipywidgets import HTML
 from ipywidgets import Layout
 from ipywidgets import Select
 from ipywidgets import VBox
+from ipywidgets import widgets
 from pycelonis import get_celonis
 from pycelonis.celonis_api.errors import PyCelonisNotFoundError
 from pycelonis.celonis_api.pql.pql import PQLFilter
 
 from one_click_analysis import utils
 from one_click_analysis.configuration.configurator_class import Configurator
-from one_click_analysis.errors import ConfiguratorNotSetError
+from one_click_analysis.feature_processing.attributes.attribute import (
+    AttributeDescriptor,
+)
 from one_click_analysis.feature_processing.feature_processor import (
     FeatureProcessor,
 )
-from one_click_analysis.process_config import process_config_utils
 from one_click_analysis.process_config.process_config import ProcessConfig
 
 
@@ -31,6 +38,7 @@ class Configuration(abc.ABC):
         self,
         configurator: Configurator,
         config_identifier: str,
+        additional_prerequsit_config_identifiers: Optional[List[str]],
         title: str,
         required: bool = False,
         caption_size: int = 14,
@@ -39,6 +47,11 @@ class Configuration(abc.ABC):
         self.configurator = configurator
 
         self.config_identifier = config_identifier
+        # Additional config identifiers that need to have an entry before the real
+        # config box is built.
+        self.additional_prerequsit_config_identifiers = (
+            additional_prerequsit_config_identifiers
+        )
         self.caption_size = caption_size
         self.caption_bold = caption_bold
         self.required = required
@@ -109,8 +122,7 @@ class Configuration(abc.ABC):
         # Create box
         self._create_config_box()
 
-        # Apply config in case there is a default configuration
-        # self.apply()
+        # Apply config in case there is a default configuration  # self.apply()
 
     def _create_config_box(self):
         """Create config box. If the prerequisites are met, the true config box is
@@ -138,6 +150,16 @@ class Configuration(abc.ABC):
             return
         self.configurator.config_dict[self.config_identifier] = self.config
         self.configurator.filter_dict[self.config_identifier] = self.filters
+
+    def _validate_additional_prerequisites(self):
+        if self.additional_prerequsit_config_identifiers is None:
+            return True
+        additional_configs_satisfied = True
+        for id in self.additional_prerequsit_config_identifiers:
+            if id not in self.configurator.config_dict:
+                additional_configs_satisfied = False
+                break
+        return additional_configs_satisfied
 
     @property
     @abc.abstractmethod
@@ -220,6 +242,7 @@ class DatePickerConfig(Configuration):
         datamodel_identifier: str,
         activity_table_identifier: str,
         config_identifier: str = "datepicker",
+        additional_prerequsit_config_identifiers: Optional[List[str]] = None,
         **kwargs,
     ):
         """
@@ -237,6 +260,7 @@ class DatePickerConfig(Configuration):
             configurator=configurator,
             config_identifier=config_identifier,
             title=title,
+            additional_prerequsit_config_identifiers=additional_prerequsit_config_identifiers,
             **kwargs,
         )
         self.datamodel_identifier = datamodel_identifier
@@ -267,7 +291,11 @@ class DatePickerConfig(Configuration):
             is not None
         )
 
-        return process_config_set and activity_table_set
+        return (
+            process_config_set
+            and activity_table_set
+            and self._validate_additional_prerequisites()
+        )
 
     def create_filter_queries(self):
         filter_start = filter_end = None
@@ -354,6 +382,7 @@ class DatamodelConfig(Configuration):
         self,
         configurator: Configurator,
         config_identifier: str = "datamodel",
+        additional_prerequsit_config_identifiers: Optional[List[str]] = None,
         celonis_login: Optional[Dict[str, str]] = None,
         **kwargs,
     ):
@@ -366,6 +395,7 @@ class DatamodelConfig(Configuration):
         super().__init__(
             configurator=configurator,
             config_identifier=config_identifier,
+            additional_prerequsit_config_identifiers=additional_prerequsit_config_identifiers,
             title=title,
             **kwargs,
         )
@@ -426,8 +456,7 @@ class DatamodelConfig(Configuration):
             return False
 
     def validate_prerequisites(self) -> bool:
-        # Does not need any prerequisites
-        return True
+        return self._validate_additional_prerequisites()
 
 
 class ActivityTableConfig(Configuration):
@@ -440,6 +469,7 @@ class ActivityTableConfig(Configuration):
         configurator: Configurator,
         datamodel_identifier: str,
         config_identifier: str = "activity_table",
+        additional_prerequsit_config_identifiers: Optional[List[str]] = None,
         **kwargs,
     ):
         if "title" in kwargs:
@@ -451,6 +481,7 @@ class ActivityTableConfig(Configuration):
         super().__init__(
             configurator=configurator,
             config_identifier=config_identifier,
+            additional_prerequsit_config_identifiers=additional_prerequsit_config_identifiers,
             title=title,
             **kwargs,
         )
@@ -506,7 +537,305 @@ class ActivityTableConfig(Configuration):
             self.datamodel_identifier in self.configurator.config_dict
             and self.configurator.config_dict[self.datamodel_identifier] is not None
         )
-        return process_config_set
+        return process_config_set and self._validate_additional_prerequisites()
+
+
+class AttributeSelectionConfig(Configuration):
+    """Configuration for selecting the activity table. The name of the activity
+    table is stored in config['activity_table_str']
+    """
+
+    def __init__(
+        self,
+        configurator: Configurator,
+        attribute_descriptions: List[AttributeDescriptor],
+        datamodel_identifier: str,
+        activity_table_identifier: str,
+        config_identifier: str = "attribute_selection",
+        additional_prerequsit_config_identifiers: Optional[List[str]] = None,
+        **kwargs,
+    ):
+        if "title" in kwargs:
+            title = kwargs["title"]
+            kwargs.pop("title")
+        else:
+            title = f"Select attributes."
+
+        super().__init__(
+            configurator=configurator,
+            config_identifier=config_identifier,
+            additional_prerequsit_config_identifiers=additional_prerequsit_config_identifiers,
+            title=title,
+            **kwargs,
+        )
+
+        self.datamodel_identifier = datamodel_identifier
+        self.activity_table_identifier = activity_table_identifier
+        self.attribute_descriptions = attribute_descriptions
+        self.local_selected_attributes = self.attribute_descriptions.copy()
+        self.local_selected_activity_cols = []  # activity_table_cols.copy()
+        self.local_selected_case_cols = {}
+        # Initialize config box
+        self._create_config_box()
+
+    def _create_initial_local_selected_case_cols(self):
+        process_config = self.configurator.config_dict[self.datamodel_identifier][
+            "process_config"
+        ]
+        activity_table_str = self.configurator.config_dict[
+            self.activity_table_identifier
+        ]["activity_table_str"]
+        tables = [
+            t.table_str
+            for t in process_config.get_case_level_tables(activity_table_str)
+        ]
+        table_dict = {t: [] for t in tables}
+        return table_dict
+
+    def _update_configurator(self):
+        self.configurator.config_dict[self.config_identifier] = {
+            "attributes": self.local_selected_attributes,
+            "activity_table_cols": self.local_selected_activity_cols,
+            "case_level_table_cols": self.local_selected_case_cols,
+        }
+
+    def _create_true_config_box(self):
+        """Create the box which contains checkboxes for all attributes including the
+        column names of activity and case table (if activity and case table attribute is
+        within fp.minor_attrs)
+
+        :return: box with the attribute selection
+        """
+        process_config = self.configurator.config_dict[self.datamodel_identifier][
+            "process_config"
+        ]
+        activity_table_str = self.configurator.config_dict[
+            self.activity_table_identifier
+        ]["activity_table_str"]
+        self.local_selected_case_cols = self._create_initial_local_selected_case_cols()
+        # dict that maps Attribute display_name to the attribute object
+        attrs_dict = {i.display_name: i for i in self.attribute_descriptions}
+
+        cbs = []
+
+        def on_checkbox_clicked(b):
+            """Define behaviour when checkbox of a "normal" attribute (not activity
+            or case column attribute) is toggled
+
+            :param b:
+            :return:
+            """
+            if b.new is False:
+                self.local_selected_attributes.remove(attrs_dict[b.owner.description])
+            else:
+                self.local_selected_attributes.append(attrs_dict[b.owner.description])
+            self._update_configurator()
+
+        for attr in self.attribute_descriptions:
+            # if the attribute is the label,
+            cb = widgets.Checkbox(
+                value=True, description=attr.display_name, indent=False
+            )
+            cb.observe(on_checkbox_clicked, "value")
+            cbs.append(cb)
+        # activity_table_cols_cat, activity_table_cols_num = \
+        #    process_config.get_categorical_numerical_columns(
+        #    activity_table_str)
+
+        # activity_table_cols = activity_table_cols_cat + activity_table_cols_num
+        # activity_table_cols = sorted(activity_table_cols)
+        activity_table = process_config.table_dict[activity_table_str]
+        cbs_activity_table = self.create_cbs_activity_case(
+            "activity", table=activity_table
+        )
+        cbs_case_tables = []
+        case_level_tables = process_config.get_case_level_tables(activity_table_str)
+        for table in case_level_tables:
+            cbs_case_table = self.create_cbs_activity_case("case", table=table)
+            cbs_case_tables.append(cbs_case_table)
+
+        cbs_activity_case_table = [cbs_activity_table] + cbs_case_tables
+        # remove None
+        cbs_activity_case_table = list(filter(None, cbs_activity_case_table))
+
+        vbox_cbs = widgets.VBox(children=cbs + cbs_activity_case_table)
+
+        # Add a title
+        html_title_str = (
+            '<span style="font-weight:bold;  font-size:16px">Attribute '
+            "selection</span>"
+        )
+        html_title = widgets.HTML(html_title_str)
+        vbox_all_attributes = widgets.VBox(children=[html_title, vbox_cbs])
+        self.config_box = vbox_all_attributes
+
+    def create_cbs_activity_case(self, table_type: str, table: Any) -> widgets.VBox:
+        """Create the checkboxes for activity or case table columns.
+
+        :param table_type: type of the table to use. Either 'case' for the case table or
+        'activity' for the activity table
+        :return: box with the column attribute checkboxes
+        """
+        if table_type == "case" and table is None:
+            raise ValueError("When table_type is 'case', table_name must not be None")
+
+        process_config = self.configurator.config_dict[self.datamodel_identifier][
+            "process_config"
+        ]
+        table_name = table.table_str
+
+        cat_columns, num_columns = process_config.get_categorical_numerical_columns(
+            table_name
+        )
+
+        columns = cat_columns + num_columns
+
+        column_dict = {col.name + " (categorical)": col.name for col in cat_columns}
+        column_dict_numerical = {
+            col.name + " (numeric)": col.name for col in num_columns
+        }
+        column_dict.update(column_dict_numerical)
+        # Configs based on table value
+        if table_type == "activity":
+            title = "Selection of Activity table columns to be usable by attributes:"
+            selected_cols = self.local_selected_activity_cols
+        elif table_type == "case":
+            title = (
+                f"Selection of {table_name} table columns to be usable by "
+                f"attributes:"
+            )
+            selected_cols = self.local_selected_case_cols[table_name]
+        else:
+            raise ValueError("table must be one of ['case', 'activity']")
+
+        # Checkboxes for columns
+        cbs = []
+
+        # Add checkbox to Select/Unselect all columms as first checkbox
+        cb_select_all = widgets.Checkbox(
+            value=True, description="Select / Unselect all", indent=False
+        )
+
+        cbs.append(cb_select_all)
+
+        # Define behaviour that happens when the Select / Unselect all checkbox is
+        # toggled
+        def select_all_changed(b, cbs: List[widgets.Checkbox]):
+            """Define behaviour that happens when the Select / Unselect all checkbox
+            is toggled.
+            If the 'select/unselect all' checkbox is toggled, all other checkboxes are
+            set to the same value
+
+            :param b: needed for observing
+            :param cbs: list with the column checkboxes
+            :return:
+            """
+            for cb in cbs:
+                cb.value = b.new
+
+        select_all_changed = functools.partial(select_all_changed, cbs=cbs)
+        cb_select_all.observe(select_all_changed, "value")
+
+        # Define behaviour that happens when a column checkbox is toggled
+        def column_cb_changed(
+            b,
+            cb_select_all: widgets.Checkbox,
+            fct_select_all: Callable,
+            selected_cols: List[str],
+            all_cols: List[str],
+        ):
+            """Define behaviour that happens when a column checkbox is toggled.
+            If a checkbox is unselected, the corresponding column is removed from the
+            list of selected columns.
+            Also, the Select/Unselect all checkbox will be unselected.
+            If a checkbox is selected, the corresponding column is appended to the
+            list of selected columns.
+            Also, the Select/Unselect all checkbox will be selected if this event
+            leads to all checkboxes selected.
+
+            :param b: needed for observing
+            :param cb_select_all: the Select/Unselect all checkbox
+            :param fct_select_all: function that is observed by the Select/Unselect
+            all checkbox
+            :param selected_cols: list with the selected columns
+            :param all_cols: list with all columns
+            :return:
+            """
+
+            # Unobserve the Select/Unselect all checkbox as an unselect of a column
+            # checkbox would lead to all checkboxes being unselected else
+            cb_select_all.unobserve(fct_select_all, "value")
+            if b.new is False:
+                selected_cols.remove(column_dict[b.owner.description])
+                cb_select_all.value = False
+            else:
+                selected_cols.append(column_dict[b.owner.description])
+                if len(selected_cols) == len(all_cols):
+                    cb_select_all.value = True
+            # Observe the Select/Unselect all checkbox
+            cb_select_all.observe(fct_select_all, "value")
+            self._update_configurator()
+
+        column_cb_changed = functools.partial(
+            column_cb_changed,
+            cb_select_all=cb_select_all,
+            fct_select_all=select_all_changed,
+            selected_cols=selected_cols,
+            all_cols=columns,
+        )
+
+        # Construct column checkboxes
+        for col_key in sorted(list(column_dict.keys())):
+            cb = widgets.Checkbox(value=False, description=col_key, indent=False)
+            cb.observe(column_cb_changed, "value")
+            cbs.append(cb)
+
+        # Create VBoxes with the checkboxes
+        layout_cb_box = widgets.Layout(max_height="300px")
+        vbox_cbs_cat = widgets.VBox(children=cbs, layout=layout_cb_box)
+
+        # Create accordion
+        acc = widgets.Accordion(children=[vbox_cbs_cat], selected_index=None)
+        acc.set_title(0, "columns")
+
+        # Add title to accordion using a VBox
+        html_title_str = (
+            '<span style="font-weight:bold;  font-size:14px">' + title + "</span>"
+        )
+        html_title = widgets.HTML(html_title_str)
+
+        vbox_col_attrs = widgets.VBox(children=[html_title, acc])
+        return vbox_col_attrs
+
+    @property
+    def requirement_met(self) -> bool:
+        try:
+            if (
+                self.configurator.config_dict[self.config_identifier]["attributes"]
+                is not None
+            ):
+                return True
+            else:
+                return False
+        except KeyError:
+            # Entry in configurator not set yet
+            return False
+
+    def validate_prerequisites(self) -> bool:
+        process_config_set = (
+            self.datamodel_identifier in self.configurator.config_dict
+            and self.configurator.config_dict[self.datamodel_identifier] is not None
+        )
+        activity_table_set = (
+            self.activity_table_identifier in self.configurator.config_dict
+            and self.configurator.config_dict[self.activity_table_identifier]
+            is not None
+        )
+        return (
+            process_config_set
+            and activity_table_set
+            and self._validate_additional_prerequisites()
+        )
 
 
 class DecisionConfig(Configuration):
