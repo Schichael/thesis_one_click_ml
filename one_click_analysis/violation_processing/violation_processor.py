3dbc504b312278e76751e793ef8b30e21080509a
Michael Schulten <michael.schulten@celonis.de>
2022-06-15 15:35:31 +0200

    feat: add computations of metrics for violations

--- one_click_analysis/violation_processing/violation_processor.py
+++ one_click_analysis/violation_processing/violation_processor.py
@@ -4,6 +4,12 @@ from typing import Optional
 import pandas as pd
 from pycelonis.celonis_api.pql import pql
 
+from one_click_analysis.feature_processing.attributes.static_attributes import (
+    CaseDurationAttribute,
+)
+from one_click_analysis.feature_processing.attributes.static_attributes import (
+    EventCountAttribute,
+)
 from one_click_analysis.process_config.process_config import ActivityTable
 from one_click_analysis.process_config.process_config import ProcessConfig
 from one_click_analysis.violation_processing.violation import Violation
@@ -37,18 +43,17 @@ class ViolationProcessor:
         """
         violations_df = self._get_violations_df()
         unique_violations = violations_df["violation"].unique()
-        value_counts = violations_df["violation"].value_counts()
         violations = []
         for violation in unique_violations:
-            violation = self._create_violation_object(
-                violation, violations_df, value_counts
-            )
+            violation = self._create_violation_object(violation, violations_df)
             if violation is not None:
                 violations.append(violation)
         return violations
 
     def _create_violation_object(
-        self, violation_str: str, violations_df: pd.DataFrame, value_counts: pd.Series
+        self,
+        violation_str: str,
+        violations_df: pd.DataFrame,
     ) -> Optional[Violation]:
         """Create a Violation object from a violation string.
 
@@ -56,12 +61,44 @@ class ViolationProcessor:
         :param violations_df: the DataFrame with the violations
         :return: Violation object if it is a supported violation. Else None
         """
+        # All cases that have this violation
+        cases_violation = violations_df[violations_df["violation"] == violation_str][
+            "case"
+        ].unique()
+        curr_violation_df = violations_df[violations_df["case"].isin(cases_violation)]
+        # All cases that don't have this violation
+        cases_violation = curr_violation_df["case"].unique()
+        curr_not_violation_df = violations_df[
+            ~violations_df["case"].isin(cases_violation)
+        ]
         # Number of cases with violation
-        num_cases = len(
-            violations_df[violations_df["violation"] == violation_str]["case"].unique()
-        )
+        num_cases = len(curr_violation_df["case"].unique())
         # Number of violation occurrences
-        num_occurrences = value_counts[violation_str]
+        num_occurrences = len(curr_violation_df)
+
+        # Case duration with violation
+        curr_violation_df_grouped = curr_violation_df.groupby("case").first()
+        avg_case_duration_with_violation = curr_violation_df_grouped[
+            "case " "duration"
+        ].mean()
+        # Case duration without violation
+        curr_not_violation_df_grouped = curr_not_violation_df.groupby("case").first()
+        avg_case_duration_without_violation = curr_not_violation_df_grouped[
+            "case " "duration"
+        ].mean()
+
+        # Number of events with violation
+        curr_violation_df_grouped = curr_violation_df.groupby("case").count()
+        mean_num_events_with_violation = curr_violation_df_grouped[
+            "case duration"
+        ].mean()
+
+        # Number of events without violation
+        curr_not_violation_df_grouped = curr_not_violation_df.groupby("case").count()
+        mean_num_events_without_violation = curr_not_violation_df_grouped[
+            "case " "duration"
+        ].mean()
+
         # Check if violation is a violating Start activity
         if violation_str.endswith(" executed as start activity"):
             violation_type = ViolationType.START_ACTIVITY
@@ -78,11 +115,18 @@ class ViolationProcessor:
         else:
             return None
 
+        metrics = {
+            "avg_case_duration_with_violation": avg_case_duration_with_violation,
+            "avg_case_duration_without_violation": avg_case_duration_without_violation,
+            "mean_num_events_with_violation": mean_num_events_with_violation,
+            "mean_num_events_without_violation": mean_num_events_without_violation,
+        }
         return Violation(
             violation_type=violation_type,
             specifics=specifics,
             num_cases=num_cases,
             num_occurrences=num_occurrences,
+            metrics=metrics,
         )
 
     def _get_violations_df(self) -> pd.DataFrame:
@@ -100,6 +144,22 @@ class ViolationProcessor:
         pql_query.add(pql.PQLColumn(query=pql_cases_str, name="case"))
         pql_violations_str = "READABLE(" + self.conformance_query_str + ")"
         pql_query.add(pql.PQLColumn(query=pql_violations_str, name="violation"))
+        case_duration_attr = CaseDurationAttribute(
+            process_config=self.process_config,
+            activity_table_str=self.activity_table_str,
+            time_aggregation="SECONDS",
+        )
+        pql_case_duration_str = case_duration_attr.pql_query.query
+        pql_query.add(
+            pql.PQLColumn(query=pql_case_duration_str, name="case " "duration")
+        )
+
+        event_count_attr = EventCountAttribute(
+            process_config=self.process_config,
+            activity_table_str=self.activity_table_str,
+        )
+        pql_event_count_str = event_count_attr.pql_query.query
+        pql_query.add(pql.PQLColumn(query=pql_event_count_str, name="event count"))
         # Add filters
         pql_query.add(self.filters)
         df = self.process_config.dm.get_data_frame(pql_query)
